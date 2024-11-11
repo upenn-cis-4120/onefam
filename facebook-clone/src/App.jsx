b3cdb99575515e62fe51f93a4031fa90483b26e2
Su Yeon Lee <suyeonlee@ist-11557.apn.wlan.private.upenn.edu>
2024-11-11 16:11:35 -0500

    changes

--- facebook-clone/src/App.jsx
+++ facebook-clone/src/App.jsx
@@ -1,12 +1,16 @@
 ﻿// src/App.jsx
 import FacebookClone from './components/FacebookClone'
 import Post from './components/Post'
+import SharePhoto from './components/SharePhoto'
+import TakePhoto from './components/TakePhoto'
 
 function App() {
     return (
         <div className="min-h-screen bg-fb-gray">
             <FacebookClone />
             <Post />
+            <SharePhoto />
+            <TakePhoto />
         </div>
     )
 }
